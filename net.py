08fdef8744ef51625f566b771abbfb33a05d0c82
jonot <48457507+jonot-cyber@users.noreply.github.com>
2020-01-20 08:41:53 -0500

    Add files via upload

--- net.py
+++ net.py
@@ -1,6 +1,5 @@
 import os, re
-var = os.popen('nmap -sn {}/{}'.format(input("Enter Network Identifier: "), input("Enter subnet mask"))).read()
-#var = os.popen('nmap -sn 192.168.0.17/24').read()
-print("Hosts found: "+var.split('\n')[-2].split(' ')[5][1:])
-print("MAC Addresses:")
-print("\n".join([x.replace("MAC Address: ","")[:17].replace("Nmap done: 256 IP","") for x in var.split('\n')[1::3][1:]]))
+var = os.popen('arp -an').read()
+print(str(len(var.split('\n')))+" devices found")
+print("MAC Addresses: ")
+print("\n".join([x.split(' ')[3] for x in var.split('\n') if x != '']))
